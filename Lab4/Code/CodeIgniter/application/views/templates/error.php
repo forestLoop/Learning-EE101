99e26d6349fa59f19a763b77ff6e3ae8e93d9ee5
keithnull <keith1126@126.com>
2018-05-22 20:41:56 +0800

    .

--- Lab4/Code/CodeIgniter/application/views/templates/error.php
+++
@@ -1,1 +0,0 @@
-<div id='noResult'><?php echo $errorMsg; ?></div>
--- Lab4/Code/CodeIgniter/application/views/templates/hint.php
+++
@@ -1,9 +0,0 @@
-<?php
-if($queryResult){
-    foreach($queryResult as $row ){
-        $resultArray[] = array('id' => $row["AuthorID"],'label'=>$row["AuthorName"] );
-    }
-    echo json_encode($resultArray);
-}
-
-?>
