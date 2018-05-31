8b57c66dec4d33665fdbd81b07a969c9dd3af2f4
keithnull <keith1126@126.com>
2018-05-31 14:23:33 +0800

    finished two thirds of the frontend of search

--- Final Project/Code/CodeIgniter/application/views/templates/result.php
+++ Final Project/Code/CodeIgniter/application/views/templates/result.php
@@ -13,8 +13,6 @@
 ?>
 
 
-
-
 <div class="searchResult">
 	<div class="overallResult"><p><?php echo "$resultNum results have been found." ?></p></div>
 
@@ -51,9 +49,9 @@
 <?php if(isset($paperNum)):?>
 	<div class="resultContainer">
 	    <div class="overallResult">
-	    	<p><?php echo "$paperNum papers have been found." ?></p>
+	    	<p><?=$paperNum?> papers have been found.</p>
 	    </div>
-	    <?php if($paperNum!=0) :?>
+		<?php if($paperNum!=0) :?>
 	    <table class="resultTable">
 	    	<tr>
 	    		<th>Paper Title</th>
@@ -62,28 +60,86 @@
 	    		<th>Citations</th>
 	    		<th>Author(s)</th>
 	    	</tr>
-		<?php foreach($paperResult as $singlePaper):?>
+			<?php foreach($paperResult as $singlePaper):?>
 			<tr>
 				<td><a href="/paper/<?=$singlePaper['paperID']?>"><?=$singlePaper["title"]?></a></td>
 				<td><?=$singlePaper["paperPublishYear"]?></td>
-				<td>
-					<a href="/conference/<?=$singlePaper['conferenceID']?>"><?$singlePaper["conferenceName"]?></a>
-				</td>
+				<td><a href="/conference/<?=$singlePaper['conferenceID']?>"><?$singlePaper["conferenceName"]?></a></td>
 				<td><?$singlePaper["citations"]?></td>
 				<td>
 					<ol>
-						<?php foreach($singlePaper["authors"] as $author):?>
+							<?php foreach($singlePaper["authors"] as $author):?>
 							<li>
 								<a href="/author/<?=$author['authorID']?>">
 									<?=$author["authorName"]?>
 								</a>
 							</li>
-						<?php endforeach;?>
+							<?php endforeach;?>
 					</ol>
 				</td>
 			</tr>
-		<?php endforeach;?>
+			<?php endforeach;?>
 	    </table>
+		<?php endif;?>
+	</div>
+<?php endif;?>
+
+<?php if(isset($conferenceNum)):?>
+	<div class="resultContainer">
+		<div class="overallResult">
+			<p><?=$conferenceNum?> conferences are found.</p>
+		</div>
+		<?php if($conferenceNum!=0):?>
+		<table class="resultTable">
+			<tr>
+				<th>Conference Name</th>
+				<th>Papers</th>
+				<th>Authors</th>
+				<th>Influence</th>
+			</tr>
+			<?php foreach($conferenceResult as $singleConference) :?>
+			<tr>
+				<td>
+					<a href="/conference/<?=$singleConference['conferenceID']?>">
+						<?=$singleConference["conferenceName"]?>
+					</a>
+				</td>
+				<td><?=$singleConference["paperNum"]?></td>
+				<td><?=$singleConference["authorNum"]?></td>
+				<td><?=$singleConference["influence"]?></td>
+			</tr>
+			<?php endforeach;?>
+		</table>
+		<?php endif;?>
+	</div>
+<?php endif;?>
+
+<?php if(isset($affiliationNum)):?>
+	<div class="resultContainer">
+		<div class="overallResult">
+			<p><?=$affiliationNum?> affiliations are found.</p>
+		</div>
+		<?php if($affiliationNum!=0):?>
+		<table class="resultTable">
+			<tr>
+				<th>Affiliation Name</th>
+				<th>Papers</th>
+				<th>Authors</th>
+				<th>Influence</th>
+			</tr>
+			<?php foreach($affiliationResult as $singleAffiliation) :?>
+			<tr>
+				<td>
+					<a href="/affiliation/<?=$singleAffiliation['affiliationID']?>">
+						<?=$singleAffiliation["affiliationName"]?>
+					</a>
+				</td>
+				<td><?=$singleAffiliation["paperNum"]?></td>
+				<td><?=$singleAffiliation["authorNum"]?></td>
+				<td><?=$singleAffiliation["influence"]?></td>
+			</tr>
+			<?php endforeach;?>
+		</table>
 		<?php endif;?>
 	</div>
 <?php endif;?>
